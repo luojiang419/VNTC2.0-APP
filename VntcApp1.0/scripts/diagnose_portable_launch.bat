044cb48239771d0604088d25af7f3c9e860c6090
luojiang419 <luojiang419@users.noreply.github.com>
2026-06-01 23:14:58 +0800

    chore: 忽略嵌入式子仓库 VntcApp1.0/

--- VntcApp1.0/scripts/diagnose_portable_launch.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-setlocal
-PowerShell -ExecutionPolicy Bypass -File "%~dp0diagnose_portable_launch.ps1" %*
--- VntcApp1.0/scripts/diagnose_windows_release.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-
-powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0diagnose_windows_release.ps1" %*
-
